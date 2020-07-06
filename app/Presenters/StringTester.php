378dfb134d35075412f9763a943733484bba14a6
Ondřej Maxa <ondrej.maxa.ih1@gmail.com>
2020-07-07 00:04:39 +0200

    command name

---
+++ app/Presenters/StringTester.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use App\Model\BS4Form;
+use Nette\Application\UI\Form as UIForm;
+use Nette\Forms\Form;
+
+final class StringTesterPresenter extends BasePresenter
+{
+    protected function createComponentTsvForm(): UIForm
+    {
+        $form = new BS4Form;
+        $form->addUpload('tsv')
+            ->setRequired('Tsv is required');
+
+        $form->addSubmit('check');
+        $form->onSuccess[] = [$this, 'tsvFormNext'];
+        return $form;
+    }
+
+    public function tsvFormNext(Form $form, \stdClass $values): void
+    {
+        $tsv = file_get_contents($values->tsv->getTemporaryFile());
+        $rawRows = preg_split('/\r\n/', $tsv);
+        $data = [];
+        foreach ($rawRows as $key => $rawRow) {
+            $rawColumns = preg_split('/\t/', $rawRow);
+            if($key != 0) {
+                $data[] = [
+                    'row' => $key+1,
+                    'key' => $rawColumns[2],
+                    'string' => $rawColumns[3],
+                    'translated' => $rawColumns[4],
+                ];
+            }
+        }
+        while (true) {
+            $filename = __DIR__.'/../../temp/' . uniqid('Sheet', true) . '.php';
+            if (!file_exists($filename)) {
+                break;
+            }
+        }
+        file_put_contents($filename, '<?php return '.var_export($data, true).';');
+        $this->redirect('StringTester:failed', $filename);
+    }
+
+    public function renderFailed($filename) {
+        $data = include $filename;
+        $failed = [];
+        foreach($data as $row) {
+            if(preg_match('/command_.*_name/', $row['key'])) {
+                if($row['string'] != $row['translated']) {
+                    $failed['Command Name'][] = $row;
+                }
+            }
+        }
+        $this->template->failed = $failed;
+    }
+}
